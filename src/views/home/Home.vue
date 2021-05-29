30a726f522d0657175d2371a74e798cc65180c35
kingshuo <1621187656@qq.com>
2021-05-29 12:04:13 +0800

    优化滚动

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,114 +1,16 @@
 <template>
   <div id="home">
     <nav-bar class="home-nave"><div slot="center">购物街</div></nav-bar>
-    <home-swiper :banners='banners'></home-swiper>
-    <recommend-view :recommends="recommends"></recommend-view>
-    <feature-view></feature-view>
-    <tab-control class="tab-control" :titles="['流行','新款','精选']"></tab-control>
-    <goods-list :goods="goods['pop'].list"></goods-list>
+    <scroll class="content" ref="scroll">
+      <home-swiper :banners='banners'></home-swiper>
+      <recommend-view :recommends="recommends"></recommend-view>
+      <feature-view></feature-view>
+      <tab-control class="tab-control" :titles="['流行','新款','精选']" @tabClick='tabClick'></tab-control>
+      <goods-list :goods="showGoods"></goods-list>
+    </scroll>
+    <!-- 监听组建的点击必须用.native，监听组件根元素的原生事件 -->
+    <back-top @click.native="backClick"></back-top>
 
-    <ul>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-    </ul>
   </div>
 </template>
 
@@ -120,6 +22,8 @@ import FeatureView from './childComps/FeatureView.vue'
 import NavBar from '../../components/common/navbar/NavBar.vue'
 import TabControl from '../../components/content/tabControl/TabControl.vue'
 import GoodsList from '../../components/content/goods/GoodsList.vue'
+import Scroll from '../../components/common/scroll/Scroll'
+import BackTop from '../../components/content/backTop/BackTop.vue'
 
 import {getHomeMultidata,getHomeGoods} from 'network/home'
 
@@ -132,6 +36,8 @@ export default {
     FeatureView,
     TabControl,
     GoodsList,
+    Scroll,
+    BackTop
   },
   data() {
     return {
@@ -141,8 +47,13 @@ export default {
         'pop': {page: 0, list: []},
         'new': {page: 0, list: []},
         'sell': {page: 0, list: []},
-      }
-
+      },
+      currentType : 'pop'
+    }
+  },
+  computed:  {
+    showGoods() {
+      return this.goods[this.currentType].list
     }
   },
   created() {
@@ -155,6 +66,16 @@ export default {
     this.getHomeGoods('sell')
   },
   methods: {
+    // 事件监听相关
+    tabClick(index) {
+      switch (index) {
+        case 0: this.currentType = 'pop';break
+        case 1: this.currentType = 'new'; break
+        case 2: this.currentType = 'sell';break
+      }
+    },
+
+    // 网络请求相关
     getHomeMultidata() {
       getHomeMultidata().then(res => {
       // console.log(res)
@@ -169,6 +90,10 @@ export default {
         this.goods[type].list.push(...res.data.list)
         this.goods[type].page += 1
     })
+    },
+
+    backClick() {
+      this.$refs.scroll.scrollTo(0, 0, 500)
     }
   }
 }
@@ -177,6 +102,8 @@ export default {
 <style scoped>
   #home {
     padding-top: 44px;
+    height: 100vh;
+    position: relative;
   }
 
   .home-nave {
@@ -192,8 +119,25 @@ export default {
 
   .tab-control {
     /* 在达到44px之前是static 达到后变成fixed */
-    position: sticky;
+    /* position: sticky; */
     top: 44px;
     z-index: 9;
   }
+
+  .content {
+    /* height: 300px; */
+    overflow: hidden;
+
+    position: absolute;
+    top: 44px;
+    bottom: 49px;
+    left: 0;
+    right: 0;
+  }
+
+  /* .content {
+    height: calc(100% - 93px);
+    overflow: hidden;
+    margin-top: 44px;
+  } */
 </style>
